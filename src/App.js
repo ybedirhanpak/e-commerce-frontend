5c0b9441af65f5d1f103886c967b0969901c41e2
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2019-07-03 10:39:43 +0300

    App updated

--- src/App.js
+++ src/App.js
@@ -1,59 +1,62 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+import Alert from 'react-bootstrap/Alert';
 
 //Components
 import SlideProduct from './components/slide-product/slide-product';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="row header-row">
-          <div className="col-3 banner">
-            <img src={logo} className="App-logo" alt="logo" />
-          </div>
-          <div className="col-6"></div>
-          <div className="col-3 sign-in">
-            {/* Sign in Component */}
-          </div>
-        </div>
-      </header>
-      <body className="App-body">
-        <div className="container-fluid App-container">
-          {/* Navbar Row */}
-          <div className="row navbar-row">
-            <div className="col">{/* Empty Col */}</div>
-            <div className="col-10 navbar-col">
-              {/* Navbar Component */}
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="row header-row">
+            <div className="col-3 banner">
+              <img src={logo} className="App-logo" alt="logo" />
             </div>
-            <div className="col"></div>
-          </div>
-          {/* Filter and Products Row */}
-          <div className="row">
-            <div className="col"></div>
-            <div className="col-2 filter">
-              {/* Filter Component */}
+            <div className="col-6"></div>
+            <div className="col-3 sign-in">
+              {/* Sign in Component */}
             </div>
+          </div>
+        </header>
+        <body className="App-body">
+          <div className="container-fluid App-container">
+            {/* Navbar Row */}
+            <div className="row navbar-row">
+              <div className="col">{/* Empty Col */}</div>
+              <div className="col-10 navbar-col">
+                {/* Navbar Component */}
+              </div>
+              <div className="col"></div>
+            </div>
+            {/* Filter and Products Row */}
+            <div className="row">
+              <div className="col"></div>
+              <div className="col-2 filter">
+                {/* Filter Component */}
+              </div>
 
-            <div className="col-8 result-products">
-              <div className="row">
-                <div className="col sort">
-                  {/* Sort Component */}
+              <div className="col-8 result-products">
+                <div className="row">
+                  <div className="col sort">
+                    {/* Sort Component */}
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col products">
+                    {/* Product Components */}
+                  </div>
                 </div>
               </div>
-              <div className="row">
-                <div className="col products">
-                  {/* Product Components */}
-                </div>
-              </div>
+              <div className="col">{/* Empty Col */}</div>
             </div>
-            <div className="col">{/* Empty Col */}</div>
           </div>
-        </div>
-      </body>
-    </div>
-  );
+        </body>
+      </div>
+    );
+  }
 }
 
-export default App;
