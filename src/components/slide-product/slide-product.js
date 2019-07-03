7e0a7d4cc5761489463da88c6f9f44d6fe8c633b
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2019-07-03 13:16:41 +0300

    Libraries added

--- src/components/slide-product/slide-product.js
+++ src/components/slide-product/slide-product.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import Carousel from 'react-bootstrap/Carousel';
 
 export default class SlideProduct extends Component {
     render() {
---
+++ src/components/top-header/top-header.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+export default class SlideProduct extends Component {
+    render() {
+        return(
+            <div id="top-header">
+				<div class="container">
+					<ul class="header-links pull-left">
+						<li><a href="#"><i class="fa fa-phone"></i> +021-95-51-84</a></li>
+						<li><a href="#"><i class="fa fa-envelope-o"></i> email@email.com</a></li>
+						<li><a href="#"><i class="fa fa-map-marker"></i> 1734 Stonecoal Road</a></li>
+					</ul>
+					<ul class="header-links pull-right">
+						<li><a href="#"><i class="fa fa-dollar"></i> USD</a></li>
+						<li><a href="#"><i class="fa fa-user-o"></i> My Account</a></li>
+					</ul>
+				</div>
+			</div>
+        );
+    }
+}
