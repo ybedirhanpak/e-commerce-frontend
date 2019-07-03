4c1647fbc20262eb15f924eb9a3998620bffbacd
Furkan <muhammet.gok@boun.edu.tr>
2019-07-03 16:14:29 +0300

    product-function-component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Product from './components/product-card'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Product from "./components/product-card";
 //Components
-import TopHeader from './components/top-header/top-header';
+import TopHeader from "./components/top-header/top-header";
 
 export default class App extends Component {
+  product = {
+    name: "Product1",
+    imgSource: "./img/product01.png",
+    discount: "%30",
+    new: false,
+    category: "computer",
+    price: "210",
+    oldPrice: "300"
+  };
   render() {
     return (
       <div>
-        <Product />
+        {Product(this.product)}
       </div>
     );
   }
 }
-
