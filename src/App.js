c18c2b52f0b75f542de63c7c014dea5003ed8600
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2019-07-03 17:43:36 +0300

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 //Components
 import MainHeader from './components/main-header/main-header'
 import NavigationBar from './components/navigation-bar/navigation-bar';
+import ProductCard from './components/product-card/index';
 
 export default class App extends Component {
   render() {
@@ -12,10 +13,8 @@ export default class App extends Component {
         <body className="App-body">
           <MainHeader/>
           <NavigationBar/>
+          <ProductCard/>
         </body>
-        
-        
-        
       </div>
     );
   }
