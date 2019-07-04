035c00de230d5d546cf214b99ae0d9259a83f9d7
Alicem <krlbl_alicem@hotmail.com>
2019-07-04 11:04:19 +0300

    navbar update

--- src/components/navigation-bar/navigation-bar.js
+++ src/components/navigation-bar/navigation-bar.js
@@ -1,22 +1,30 @@
 import React, {Component} from 'react';
+import Kategori from '../kategori-drop/kategori'
 
 export default class NavigationBar extends Component {
     render() {
         return (
-            <nav id="navigation">
-            <div className="container">
-              <div id="responsive-nav">
-                <ul className="main-nav nav navbar-nav">
-                  <li className="active"><a href="#">Home</a></li>
-                  <li><a href="#">Hot Deals</a></li>
-                  <li><a href="#">Categories</a></li>
-                  <li><a href="#">Laptops</a></li>
-                  <li><a href="#">Smartphones</a></li>
-                  <li><a href="#">Cameras</a></li>
-                  <li><a href="#">Accessories</a></li>
-                </ul>
+            <nav id="navigation" className="navbar navbar-default navbar-static">
+              <div className="container">
+                <div id="responsive-nav" className="collapse navbar-collapse js-navbar-collapse">
+                  <ul className="main-nav nav navbar-nav">
+                    <li className=""><a href="#">Home</a></li>
+                    <Kategori tag="Teknoloji"
+                    />
+                    <Kategori tag="Kadın"
+                    />
+                    
+                    <Kategori tag="Erkek"
+                    />
+                    <Kategori tag="Çocuk"
+                    />
+                      <Kategori tag="Ayakkabı & Çanta"
+                  />
+                    <li><a href="#">İletişim</a></li>
+                
+                  </ul>                
+                </div>
               </div>
-            </div>
           </nav>
         );
     }
