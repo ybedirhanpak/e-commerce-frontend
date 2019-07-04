88a1191b361081666844224162652172068084c5
Furkan Gök <muhammet.gok@boun.edu.tr>
2019-07-04 16:21:38 +0300

    dropdown-toggle

--- src/components/city-dropdown/box.js
+++ src/components/city-dropdown/box.js
@@ -1,80 +1,50 @@
-import React, { Component } from 'react'
-import './style.css'
+import React, { Component } from "react";
+import "./style.css";
 
 class Box extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showItems: false,
+      selectedCity: null
+    };
+    this.selectCity = this.selectCity.bind(this);
+  }
 
-    render() {
-        const dataList = this.props.data.map((element) =>
-            <li className="list-group-item my-item" key={element.name} value={element} id={element.name}>{element.name}</li>
-        );
+  dropDown = () => {
+    this.setState(prevState => ({
+      showItems: !prevState.showItems
+    }));
+  };
 
-        return (
+  selectCity(item) {
+    this.setState({
+      selectedCity: item
+    });
+  }
 
-            <div className="list-group">
-               {dataList}   
-            </div>
-
-<div class="aside">
-<h3 class="aside-title" onClick={()=>this.props.handleClick(element)}  >Categories</h3>
-<div class="checkbox-filter">
-
-    <div class="input-checkbox">
-        <input type="checkbox" id="category-1">
-        <label for="category-1">
-            <span></span>
-            Laptops
-            <small>(120)</small>
+  render() {
+    const cities = this.props.data.map((element, index) => (
+      <div class="input-checkbox">
+        <input type="checkbox" id={"category-" + index} />
+        <label for={"category-" + index}>
+          <span />
+          {element.name}
         </label>
-    </div>
+      </div>
+    ));
 
-    <div class="input-checkbox">
-        <input type="checkbox" id="category-2">
-        <label for="category-2">
-            <span></span>
-            Smartphones
-            <small>(740)</small>
-        </label>
-    </div>
-
-    <div class="input-checkbox">
-        <input type="checkbox" id="category-3">
-        <label for="category-3">
-            <span></span>
-            Cameras
-            <small>(1450)</small>
-        </label>
-    </div>
-
-    <div class="input-checkbox">
-        <input type="checkbox" id="category-4">
-        <label for="category-4">
-            <span></span>
-            Accessories
-            <small>(578)</small>
-        </label>
-    </div>
-
-    <div class="input-checkbox">
-        <input type="checkbox" id="category-5">
-        <label for="category-5">
-            <span></span>
-            Laptops
-            <small>(120)</small>
-        </label>
-    </div>
-
-    <div class="input-checkbox">
-        <input type="checkbox" id="category-6">
-        <label for="category-6">
-            <span></span>
-            Smartphones
-            <small>(740)</small>
-        </label>
-    </div>
-</div>
-</div>
-        )
-    }
+    return (
+      <div id="aside" class="col-md-3">
+        <div class="aside">
+          <h3 class="aside-title" onClick={this.dropDown}>
+            Categories
+          </h3>
+          {this.state.showItems && <div class="checkbox-filter">{cities}</div>}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Box
+export default Box;
