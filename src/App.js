3e396f39f41b6925f707634e9062b7d64e445153
Furkan <muhammet.gok@boun.edu.tr>
2019-07-04 10:15:18 +0300

    product-class-component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-
-
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 //Components
-import MainHeader from './components/main-header/main-header'
-import NavigationBar from './components/navigation-bar/navigation-bar';
-import ProductCard from './components/product-card/index';
-
+import MainHeader from "./components/main-header/main-header";
+import NavigationBar from "./components/navigation-bar/navigation-bar";
+import ProductCard from "./components/product-card/index";
+import Filter from "./components/filter";
 
 export default class App extends Component {
   product = {
@@ -21,12 +19,12 @@ export default class App extends Component {
   };
   render() {
     return (
-
       <div className="App">
         <body className="App-body">
-          <MainHeader/>
-          <NavigationBar/>
-          <ProductCard/>
+          <MainHeader />
+          <NavigationBar />
+          <Filter />
+          <ProductCard product={this.product} />
         </body>
       </div>
     );
