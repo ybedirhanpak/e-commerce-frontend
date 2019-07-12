9db9e82b9f4c76f84567d5bc5ca85157e1854235
aliickrr <alickrr11@gmail.com>
2019-07-12 15:38:25 +0300

    product-detail-component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "./utils/style.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
