7b1e446bd4dbb2d74dab760baf7735715278198c
Furkan Gök <muhammet.gok@boun.edu.tr>
2019-07-12 10:05:38 +0300

    url-helper

--- src/services/url-helper.js
+++ src/services/url-helper.js
@@ -16,3 +16,20 @@ export const PostWithUrlBody=(url,bodyIn)=>{
     });
     return request;
 };
+
+export const GetWithUrlBody=(url=>{
+    var request = fetch(url, {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json"
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        redirect: "follow", // manual, *follow, error
+        referrer: "no-referrer", // no-referrer, *client
+        //body: JSON.stringify(bodyIn), // body data type must match "Content-Type" header
+    });
+    return request;
+};
