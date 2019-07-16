f49a4b80f859ab7047cbd14063eaed591440745d
Furkan Gök <muhammet.gok@boun.edu.tr>
2019-07-16 16:29:41 +0300

    now users can give feedbacks

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ import Footer from "./components/bottom-footer/footer";
 //Containers
 import HomeContainer from "./containers/home-container/home-container";
 import ProductDetailedContainer from "./containers/product-detailed-container/product-detailed-container";
-import AboutUsContainer from './containers/about-us-container/about-us-container';
-import ContactUsContainer from './containers/contact-us-container/contact-us-container';
-import PrivacyPolicyContainer from './containers/privacy-policy-container/privacy-policy-container';
-import TermsContainer from './containers/terms-container/terms-container';
+import AboutUsContainer from "./containers/about-us-container/about-us-container";
+import ContactUsContainer from "./containers/contact-us-container/contact-us-container";
+import PrivacyPolicyContainer from "./containers/privacy-policy-container/privacy-policy-container";
+import TermsContainer from "./containers/terms-container/terms-container";
 import AddressLocationContainer from "./containers/address-location-container/address-location-container";
 import PhoneContactContainer from "./containers/phone-contact-container/phone-contact-container";
 import EmailContactContainer from "./containers/email-contact-container/email-contact-container";
@@ -35,30 +35,32 @@ export default class App extends Component {
             <MainHeader />
             <NavigationBar />
             <Switch>
-              <Route exact path="/" render={() => (
-                <Redirect to='home'/>
-              )}/>
-              <Route path="/home" component={HomeContainer} exact/>
+              <Route exact path="/" render={() => <Redirect to="home" />} />
+              <Route path="/home" component={HomeContainer} exact />
 
-              <Route path="/productDetailed/:productId" render={(props) => {
-                const productId = props.match.params.productId;
-                return(
-                  <ProductDetailedContainer productId={productId}/>
-                )
-              }}/>
+              <Route
+                path="/productDetailed/:productId"
+                render={props => {
+                  const productId = props.match.params.productId;
+                  return <ProductDetailedContainer productId={productId} />;
+                }}
+              />
 
-              <Route path="/about" component={AboutUsContainer}/>
-              <Route path="/contact" component={ContactUsContainer}/>
-              <Route path="/privacy" component={PrivacyPolicyContainer}/>
-              <Route path="/terms" component={TermsContainer}/>
-              <Route path="/address-location" component={AddressLocationContainer}/>
-              <Route path="/phone-contact" component={PhoneContactContainer}/>
-              <Route path="/email-contact" component={EmailContactContainer}/>
-              <Route path="/account" component={MyAccountContainer}/>
-              <Route path="/cart" component={WiewCardContainer}/>
-              <Route path="/track" component={TrackMyOrderContainer}/>
-              <Route path="/help" component={HelpContainer}/>
-              <Route component={NoPageContainer}/>
+              <Route path="/about" component={AboutUsContainer} />
+              <Route path="/contact" component={ContactUsContainer} />
+              <Route path="/privacy" component={PrivacyPolicyContainer} />
+              <Route path="/terms" component={TermsContainer} />
+              <Route
+                path="/address-location"
+                component={AddressLocationContainer}
+              />
+              <Route path="/phone-contact" component={PhoneContactContainer} />
+              <Route path="/email-contact" component={EmailContactContainer} />
+              <Route path="/account" component={MyAccountContainer} />
+              <Route path="/cart" component={WiewCardContainer} />
+              <Route path="/track" component={TrackMyOrderContainer} />
+              <Route path="/help" component={HelpContainer} />
+              <Route component={NoPageContainer} />
             </Switch>
             <Footer />
           </BrowserRouter>
