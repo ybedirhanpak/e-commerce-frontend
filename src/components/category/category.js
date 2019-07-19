a890cc80fb571f0f4e0fc5f8caf7d404eb7c73a5
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2019-07-19 14:49:12 +0300

    category updated

--- src/components/category/category.js
+++ src/components/category/category.js
@@ -12,6 +12,15 @@ export default class Category extends Component {
     this.generateSubCategories = this.generateSubCategories.bind(this);
   }
 
+  generateSection = (index) => {
+    return (
+      <div>
+        <li className="dropdown-header">{this.props.sections[index].header.name}</li>
+        {this.generateSubCategories(this.props.sections[index])}
+      </div>
+    )
+  } 
+
   generateSubCategories = (section) => {
     let resultList = [];
     for (var i = 0; i < section.subcategories.length; i++) {
@@ -29,9 +38,6 @@ export default class Category extends Component {
     return resultList;
   };
 
-  generateLink = (mainCategory, section) => {
-    return '/' + mainCategory.path + '/' + section.header.path + '/' + section.subcategory.path;
-  }
   render() {
     console.log("category props", this.props);
     return (
@@ -40,52 +46,57 @@ export default class Category extends Component {
           {/* Main Hedaer */}
           {this.props.mainCategory.name} <b className="caret" />
         </a>
-
         <ul className="dropdown-menu dropdown-menu-large row">
           <li className="col-sm-3">
             <ul>
               {/* Section A */}
-              <li className="dropdown-header">{this.props.sections[0].header.name}</li>
-              {this.generateSubCategories(this.props.sections[0])}
-
+              {
+                (this.props.sections.length > 0) ? (this.generateSection(0)) : (null)
+              }
               <li className="divider" />
 
               {/* Section B */}
-              <li className="dropdown-header">{this.props.sections[1].header.name}</li>
-              {this.generateSubCategories(this.props.sections[1])}
+              {
+                (this.props.sections.length > 1) ? (this.generateSection(1)) : (null)
+              }
             </ul>
           </li>
           <li className="col-sm-3">
             <ul>
               {/* Section C */}
-              <li className="dropdown-header">{this.props.sections[2].header.name}</li>
-              {this.generateSubCategories(this.props.sections[2])}
+              {
+                (this.props.sections.length > 2) ? (this.generateSection(2)) : (null)
+              }
 
               <li className="divider" />
 
               {/* Section D */}
-              <li className="dropdown-header">{this.props.sections[3].header.name}</li>
-              {this.generateSubCategories(this.props.sections[3])}
+              {
+                (this.props.sections.length > 3) ? (this.generateSection(3)) : (null)
+              }
             </ul>
           </li>
           <li className="col-sm-3">
             <ul>
               {/* Section E */}
-              <li className="dropdown-header">{this.props.sections[4].header.name}</li>
-              {this.generateSubCategories(this.props.sections[4])}
+              {
+                (this.props.sections.length > 4) ? (this.generateSection(4)) : (null)
+              }
 
               <li className="divider" />
 
               {/* Section F */}
-              <li className="dropdown-header">{this.props.sections[5].header.name}</li>
-              {this.generateSubCategories(this.props.sections[5])}
+              {
+                (this.props.sections.length > 5) ? (this.generateSection(5)) : (null)
+              }
             </ul>
           </li>
           <li className="col-sm-3">
             <ul>
               {/* Section G */}
-              <li className="dropdown-header">{this.props.sections[6].header.name}</li>
-              {this.generateSubCategories(this.props.sections[6])}
+              {
+                (this.props.sections.length > 6) ? (this.generateSection(6)) : (null)
+              }
             </ul>
           </li>
         </ul>
