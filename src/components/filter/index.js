a5e857980c955cf754def50a37b778031b1ba3fd
Furkan Gök <muhammet.gok@boun.edu.tr>
2019-07-25 11:51:11 +0300

    minor changes

--- src/components/filter/index.js
+++ src/components/filter/index.js
@@ -1,27 +1,28 @@
 import React, { Component } from "react";
-import './style.css'
+import "./style.css";
 
 //Components
 import SelectBox from "../select-box";
-import Box from '../city-dropdown/box'
-import CategorySelectBox from '../category-select-box/category-select-box';
+import Box from "../city-dropdown/box";
+import CategorySelectBox from "../category-select-box/category-select-box";
+import PriceFilter from "../price-filter/price-filter";
 
 export default class Filter extends Component {
   render() {
     return (
       <div className="aside-filter">
-        <CategorySelectBox data={this.categories} currentCategories={this.props.currentCategories} />
-        <SelectBox data={this.categories} />
-        <PriceFilter url={this.props.url}/>
-               
-        <Box data={this.cities}/>
-       
-        <SelectBox data={this.brands} />
-        
+        <CategorySelectBox
+          data={this.categories}
+          currentCategories={this.props.currentCategories}
+        />
+        <PriceFilter url={this.props.url} />
+
+        <Box data={this.props.cityList} title="CITIES" />
+
+        <SelectBox data={this.props.brandList} title="BRANDS" />
       </div>
     );
   }
-
 
   categories = {
     title: "CATEGORIES",
@@ -82,21 +83,21 @@ export default class Filter extends Component {
     ]
   };
 
-  cities= [
+  cities = [
     {
-        "name": "İstanbul"
+      name: "İstanbul"
     },
     {
-        "name": "Hatay"
+      name: "Hatay"
     },
     {
-        "name": "Ankara"
+      name: "Ankara"
     },
     {
-        "name": "Bursa"
+      name: "Bursa"
     },
     {
-        "name": "İzmir"
+      name: "İzmir"
     }
-]
+  ];
 }
