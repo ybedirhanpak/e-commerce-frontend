cab634c52ff2be4e179ec8ee32c18cf8e470d50d
aliickrr <alickrr11@gmail.com>
2019-07-29 14:52:05 +0300

    size-options

--- src/components/product-details/product-details.js
+++ src/components/product-details/product-details.js
@@ -1,54 +1,166 @@
 import React, { Component } from "react";
 import { isNullOrUndefined } from "util";
+import LoadingSpinner from "../loading-spinner/loading-spinner";
 
-export default class ProductDetails extends Component {
-	render() {
-		const product = this.props.product;
-		return (
-			<div className="product-details">
-				<h2 className="product-name">{product.name}</h2>
-				<div>
-					<div className="product-rating">
-						<i className="fa fa-star"></i>
-						<i className="fa fa-star"></i>
-						<i className="fa fa-star"></i>
-						<i className="fa fa-star"></i>
-						<i className="fa fa-star-o"></i>
-					</div>
-					<a className="review-link" href="add-review">{(isNullOrUndefined(this.props.product.reviews) ? "0" : this.props.product.reviews.length)}Review(s) | Add your review</a>
-				</div>
-				<div>
-					<h3 className="product-price">{'$'+ product.price }<del className="product-old-price">{'$'+ product.oldPrice}</del></h3>
-					<span className="product-available">{product.quantity > 0 ? 'In Stock (' + product.quantity + ')' : 'Not In Stock'}</span>
-				</div>
-				<p>{product.description}</p>
-				<div className="product-options">
-					<label>
-						Size
-								<select className="input-select">
-							<option value="0">X</option>
-						</select>
-					</label>
-					<label>
-						Color
-								<select className="input-select">
-							<option value="0">Red</option>
-						</select>
-					</label>
-				</div>
+//Redux
+import { connect } from "react-redux";
+import { actionCreators } from "../../redux/cart/actions";
+import { isEmptyTypeAnnotation } from "@babel/types";
 
-				<div className="add-to-cart">
-					<div className="qty-label">
-						Qty
-								<div className="input-number">
-							<input type="number" />
-							<span className="qty-up">+</span>
-							<span className="qty-down">-</span>
-						</div>
-					</div>
-					<button className="add-to-cart-btn"><i className="fa fa-shopping-cart"></i> add to cart</button>
-				</div>
-			</div>
-		);
-	}
+class ProductDetails extends Component {
+  //SİZE SEÇMEYİ BİTİR
+  constructor(props) {
+    super(props);
+    this.state = {
+      quantity: 1,
+      size: this.props.product.sizeOptions
+        ? this.props.product.sizeOptions[0]
+        : "Medium"
+    };
+
+    this.createOption = this.createOption.bind(this);
+    this.handleAddtoChart = this.handleAddtoChart.bind(this);
+    this.createAverageOfStar = this.createAverageOfStar.bind(this);
+    this.quantityOnChangeHandler = this.quantityOnChangeHandler.bind(this);
+    this.isEmpty = this.isEmpty.bind(this);
+    this.onChangeOptions = this.onChangeOptions.bind(this);
+  }
+  createAverageOfStar = product => {
+    let children = [];
+    let div = [];
+    let sum = 0;
+    console.log(product);
+    for (let i = 0; i < product.stars; i++) {
+      children.push(<i className="fa fa-star" />);
+    }
+    for (let j = product.stars; j < 5; j++) {
+      children.push(<i className="fa fa-star-o" />);
+    }
+    div.push(<div className="product-rating">{children}</div>);
+    console.log(product.stars);
+    return div;
+  };
+
+  isEmpty(obj) {
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) return false;
+    }
+    return true;
+  }
+  createOption = product => {
+    console.log(this.props);
+    let sizeOptions = [];
+    console.log(this.props.product);
+    if (
+      isNullOrUndefined(this.props.product) ||
+      this.isEmpty(this.props.product)
+    ) {
+      return <LoadingSpinner />;
+    } else {
+      console.log(this.props.product, product);
+      product.sizeOptions.forEach(element => {
+        sizeOptions.push(
+          <option name="size" value={element}>
+            {element}
+          </option>
+        );
+      });
+      console.log(sizeOptions);
+      return sizeOptions;
+    }
+  };
+
+  onChangeOptions(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  quantityOnChangeHandler(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({ quantity: Number(value) });
+  }
+
+  handleAddtoChart = () => {
+    this.props.addtoCART({
+      id: this.props.product.id,
+      img: this.props.product.imgSource,
+      name: this.props.product.name,
+      rawPrice: this.props.product.price,
+      quantity: this.state.quantity,
+      price: String(Number(this.props.product.price) * this.state.quantity),
+      oldPrice: this.props.product.oldPrice,
+      size: this.state.size
+    });
+  };
+
+  render() {
+    const product = this.props.product;
+    console.log("render", this.state);
+    return (
+      <div className="product-details">
+        <h2 className="product-name">{product.name}</h2>
+        <div>
+          <a className="review-link" href="add-review">
+            {isNullOrUndefined(this.props.product.reviews)
+              ? "0"
+              : this.props.product.reviews.length}
+            Review(s) | Add your review
+          </a>
+        </div>
+        <div>
+          <h3 className="product-price">
+            {"$" + product.price}
+            <del className="product-old-price">{"$" + product.oldPrice}</del>
+          </h3>
+          <span className="product-available">
+            {product.quantity > 0
+              ? "In Stock (" + product.quantity + ")"
+              : "Not In Stock"}
+          </span>
+        </div>
+        <p>{product.description}</p>
+        <div className="product-options">
+          <label>
+            Size
+            <select
+              className="input-select"
+              name="size"
+              onChange={this.onChangeOptions}
+            >
+              {this.createOption(product)}
+            </select>
+          </label>
+        </div>
+        <div className="add-to-cart">
+          <div className="qty-label">
+            Qty
+            <div className="input-number">
+              <input
+                name="quantity"
+                onChange={this.quantityOnChangeHandler}
+                type="number"
+                placeholder={1}
+              />
+              <span className="qty-up">+</span>
+              <span className="qty-down">-</span>
+            </div>
+          </div>
+          <button onClick={this.handleAddtoChart} className="add-to-cart-btn">
+            <i className="fa fa-shopping-cart" /> add to cart
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
+
+const mapDispatchToProps = {
+  addtoCART: actionCreators.addtoCART
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(ProductDetails);
--- src/redux/cart/reducers.js
+++ src/redux/cart/reducers.js
@@ -1,97 +1,120 @@
 import { actionTypes } from "./actions";
 
 const initialState = {
-    anyProduct: false,
-    totalPrice: 0,
-    productsList: []
-}
+  anyProduct: false,
+  totalPrice: 0,
+  productsList: []
+};
 
 function cartReducer(state = initialState, action) {
-    switch (action.type) {
-        case actionTypes.DECREASE_FROMCART:
-            if(state.productsList.length > 0){
-                for(let i =0; i < state.productsList.length; i++){
-                    if(state.productsList[i].id ===action.payload.id){
-                        let data = {
-                            id:state.productsList[i].id,
-                            img:state.productsList[i].id,
-                            name:state.productsList[i].id,
-                            quantity:state.productsList[i].quantity - 1,
-                            price:String(Number(state.productsList[i].price) + Number(action.payload.price))
+  switch (action.type) {
+    case actionTypes.DECREASE_FROMCART:
+      if (state.productsList.length > 0) {
+        for (let i = 0; i < state.productsList.length; i++) {
+          if (state.productsList[i].id === action.payload.id) {
+            let data = {
+              id: state.productsList[i].id,
+              img: state.productsList[i].id,
+              name: state.productsList[i].id,
+              quantity: state.productsList[i].quantity - 1,
+              price: String(
+                Number(state.productsList[i].price) +
+                  Number(action.payload.price)
+              )
+            };
+            let tmp = state.productsList.filter(
+              product => product.id !== state.productsList[i].id
+            );
+            tmp.splice(i, 0, data);
+            return {
+              ...state,
+              anyProduct: true,
+              totalPrice: state.totalPrice + Number(action.payload.price),
+              productsList: tmp
+            };
+          }
+        }
+      }
+    case actionTypes.ADD_TOCART:
+      if (state.productsList.length > 0) {
+        let index;
+        for (let i = 0; i < state.productsList.length; i++) {
+          if (
+            state.productsList[i].id === action.payload.id &&
+            state.productsList[i].size === action.payload.size
+          ) {
+            let data = {
+              id: state.productsList[i].id,
+              img: state.productsList[i].img,
+              name: state.productsList[i].name,
+              quantity:
+                state.productsList[i].quantity + action.payload.quantity,
+              rawPrice: state.productsList[i].rawPrice,
+              price: String(
+                Number(state.productsList[i].price) +
+                  Number(state.productsList[i].rawPrice) *
+                    Number(action.payload.quantity)
+              ),
+              oldPrice: state.productsList[i].oldPrice,
+              size: state.productsList[i].size
+            };
 
-                        }
-                        let tmp = state.productsList.filter(product => product.id !== state.productsList[i].id);
-                        tmp.splice(i,0,data);
-                        return {
-                            ...state,
-                            anyProduct: true,
-                            totalPrice: state.totalPrice + Number(action.payload.price),
-                            productsList: tmp
-                        }
-                    }
-                }
-            }
-        case actionTypes.ADD_TOCART:
-            if (state.productsList.length > 0) {
-                for (let i = 0; i < state.productsList.length; i++) {
-                    if (state.productsList[i].id === action.payload.id) {
-                        let data = {
-                            id: state.productsList[i].id,
-                            img: state.productsList[i].img,
-                            name: state.productsList[i].name,
-                            quantity: state.productsList[i].quantity + action.payload.quantity,
-                            rawPrice: state.productsList[i].rawPrice,
-                            price: String(Number(state.productsList[i].price) + 
-                                Number(state.productsList[i].rawPrice) * Number(action.payload.quantity)),
-                            oldPrice: state.productsList[i].oldPrice
-                        }
-                        let tmp = state.productsList.filter(product => product.id !== state.productsList[i].id);
-                        if(data.quantity > 0) {
-                            tmp.splice(i,0,data);
-                        }
-                        return {
-                            ...state,
-                            anyProduct: true,
-                            totalPrice: state.totalPrice + Number(state.productsList[i].rawPrice) * Number(action.payload.quantity),
-                            productsList: tmp
-                        }
-                    }
-                }
-                return {
-                    ...state,
-                    anyProduct: true,
-                    totalPrice: state.totalPrice + Number(action.payload.price),
-                    productsList: [...state.productsList, action.payload]
-                }
+            let tmp = state.productsList;
+            tmp.splice(i, 1, data);
+            return {
+              ...state,
+              anyProduct: true,
+              totalPrice:
+                state.totalPrice +
+                Number(state.productsList[i].rawPrice) *
+                  Number(action.payload.quantity),
+              productsList: tmp
+            };
+          }
+        }
 
-            } else {
-                return {
-                    ...state,
-                    anyProduct: true,
-                    totalPrice: state.totalPrice + Number(action.payload.price),
-                    productsList: [...state.productsList, action.payload]
-                }
-            }
+        return {
+          ...state,
+          anyProduct: true,
+          totalPrice:
+            state.totalPrice +
+            Number(action.payload.rawPrice) * Number(action.payload.quantity),
+          productsList: [...state.productsList, action.payload]
+        };
+      } else {
+        return {
+          ...state,
+          anyProduct: true,
+          totalPrice:
+            state.totalPrice +
+            Number(action.payload.rawPrice) * Number(action.payload.quantity),
+          productsList: [...state.productsList, action.payload]
+        };
+      }
 
-        case actionTypes.DELETE_FROMCART:
-            let sum = state.totalPrice - Number(action.payload.price);
-            if (sum === 0) {
-                return {
-                    totalPrice: sum,
-                    anyProduct: false,
-                    productsList: state.productsList.filter(product => product.id !== action.payload.id)
-                }
-            } else {
-                return {
-                    totalPrice: sum,
-                    anyProduct: true,
-                    productsList: state.productsList.filter(product => product.id !== action.payload.id)
-                }
-            }
+    case actionTypes.DELETE_FROMCART:
+      let sum = state.totalPrice - Number(action.payload.price);
+      if (sum === 0) {
+        return {
+          totalPrice: sum,
+          anyProduct: false,
+          productsList: state.productsList.filter(
+            product => product.id !== action.payload.id
+          )
+        };
+      } else {
+        return {
+          totalPrice: sum,
+          anyProduct: true,
+          productsList: state.productsList.filter(
+            product => product.id !== action.payload.id
+          )
+        };
+      }
 
-        default:
-            return state
-    }
+    default:
+      return state;
+  }
 }
 
-export default cartReducer
+export default cartReducer;
